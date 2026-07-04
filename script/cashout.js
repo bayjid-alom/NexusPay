05782b78beceab5b6773dcf1117ac5c3ab6c8a39
bayjidalom <bayjidalom1005@gmail.com>
2026-07-04 12:56:00 +0600

    feat: enhance home page UI and implement cash out functionality

--- script/cashout.js
+++ script/cashout.js
@@ -6,20 +6,26 @@ document.getElementById("cashout-withdraw-btn").addEventListener("click", functi
         return;
     }
 
-    const balance = getValueFromInput("balance")
-    const cashoutAmount = getValueFromInput("cashout-amount");
+    const balanceElement = document.getElementById("balance")
 
-    const newBalance = balance - Number(cashoutAmount);
-    if(newBalance < 0){
+    let balance = getBalance("balance")
+    const cashoutAmount = getValueFromInput("cashout-amount");
+    let newBalance = balance - Number(cashoutAmount);
+    console.log(newBalance)
+
+    if (newBalance < 0) {
         alert("Invalid Amount!");
         return;
     }
 
 
 
+
     const pin = getValueFromInput("cashout-pin");
     if (pin === "1234") {
         alert("Cashout Successfully");
+        // balanceElement.innerText = newBalance;
+        setBalance(newBalance)
     }
     else {
         alert("Cashout Failed!");
--- script/machine.js
+++ script/machine.js
@@ -1,4 +1,3 @@
-
 function getValueFromInput(id) {
     const input = document.getElementById(id);
     const value = input.value;
@@ -8,6 +7,16 @@ function getValueFromInput(id) {
 
 
 function getBalance(id) {
-    const balance = document.getElementById(id);
+    const balanceElem = document.getElementById(id);
+    const balance = balanceElem.innerText;
+
+    console.log("Amount :", balance)
     return Number(balance);
+}
+
+
+
+function setBalance(value) {
+    const balanceElement = document.getElementById("balance");
+    balanceElement.innerText = value;
 }
