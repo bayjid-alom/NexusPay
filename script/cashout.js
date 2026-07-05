571c28508f42c35b1f16289ace622449090748fa
bayjidalom <bayjidalom1005@gmail.com>
2026-07-05 18:14:42 +0600

    feat: track transaction history

--- script/cashout.js
+++ script/cashout.js
@@ -23,10 +23,32 @@ document.getElementById("cashout-withdraw-btn").addEventListener("click", functi
 
     const pin = getValueFromInput("cashout-pin");
     if (pin === "1234") {
-        alert("Cashout Successfully");
+        alert(`Cash Out Successful! $${cashoutAmount} has been withdrawn from your account to ${number} at ${new Date().toLocaleString()}.`);
         // balanceElement.innerText = newBalance;
         setBalance(newBalance)
+
+
+        // ❗ Transaction
+        // step-1. find the container
+        const historyContainer = document.getElementById("history-container")
+
+        // step-2. create an element
+        const divElement = document.createElement("div")
+
+        // step-3. write innerHTML
+        divElement.innerHTML = `
+         <div
+            class="history-card bg-base-100 text-neutral/80 text-sm leading-6 rounded-md border border-gray-200 p-4 transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-md">
+
+            Cash Out Successful! $${cashoutAmount} has been withdrawn from your account to ${number} at ${new Date().toLocaleString()}.
+        </div>
+        
+        `;
+
+        // step-4. append the child to the parent
+        historyContainer.appendChild(divElement)
     }
+    
     else {
         alert("Cashout Failed!");
         return;
